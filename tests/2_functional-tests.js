9511e11a2481bb62d5908b8fa8d083a6764807ba
KING <abayemiracle@gmail.com>
2021-02-09 09:26:13 +0100

    unit testing and functional testing update

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -14,8 +14,8 @@ suite("Functional Tests", function () {
         .request(server)
         .get("/hello")
         .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, "hello Guest");
+          assert.equal(res.status, 200);
+          assert.equal(res.text, "hello Guest");
           done();
         });
     });
@@ -25,8 +25,8 @@ suite("Functional Tests", function () {
         .request(server)
         .get("/hello?name=xy_z")
         .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, "hello xy_z");
+          assert.equal(res.status, 200);
+          assert.equal(res.text, "hello xy_z");
           done();
         });
     });
@@ -35,16 +35,28 @@ suite("Functional Tests", function () {
       chai
         .request(server)
         .put("/travellers")
-
+				.send({surname: "da Verrazzano"})
         .end(function (err, res) {
-          assert.fail();
-
+          assert.equal(res.status, 200);
+					assert.equal(res.type, 'application/json');
+					assert.equal(res.body.name, 'Giovanni');
+					assert.equal(res.body.surname, 'da Verrazzano');
           done();
         });
+				 });
     });
     // #4
     test('send {surname: "da Verrazzano"}', function (done) {
-      assert.fail();
+  chai
+    .request(server)
+    .put('/travellers')
+    .send({ surname: 'da Verrazzano' })
+    /** place your tests inside the callback **/
+    .end(function(err, res) {
+      assert.equal(res.status, 200);
+      assert.equal(res.type, 'application/json', 'Response should be json');
+      assert.equal(res.body.name, 'Giovanni');
+      assert.equal(res.body.surname, 'da Verrazzano');
 
       done();
     });
@@ -53,22 +65,39 @@ suite("Functional Tests", function () {
 
 const Browser = require("zombie");
 
+Browser.site = "https://boilerplate-mochachai.pedrojavierjav1.repl.co";
+
 suite("Functional Tests with Zombie.js", function () {
+
+  const browser = new Browser();
+
+  suiteSetup(function(done) {
+    return browser.visit('/', done);
+  });
 
   suite('"Famous Italian Explorers" form', function () {
     // #5
     test('submit "surname" : "Colombo" - write your e2e test...', function (done) {
       browser.fill("surname", "Colombo").pressButton("submit", function () {
-        assert.fail();
-
+        browser.assert.success();
+        browser.assert.text('span#name', 'Cristoforo');
+        browser.assert.text('span#surname', 'Colombo');
+        browser.assert.element('span#dates', 1);
         done();
       });
     });
+		
+	
+		
     // #6
     test('submit "surname" : "Vespucci" - write your e2e test...', function (done) {
-      assert.fail();
-
-      done();
+      browser.fill("surname", "Vespucci").pressButton("submit", function () {
+        browser.assert.success();
+        browser.assert.text('span#name', 'Amerigo');
+        browser.assert.text('span#surname', 'Vespucci');
+        browser.assert.element('span#dates', 1);
+        done();
+      });
     });
   });
 });
